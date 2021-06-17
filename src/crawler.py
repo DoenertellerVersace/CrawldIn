c90b79795343a84e5afa342e823ac5459cc36567
KaeseKartoffel <jakob.hadulla@gmail.com>
2021-06-17 23:12:20 +0200

    test

--- src/crawler.py
+++ src/crawler.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium.webdriver.common.keys import Keys
-import json
+
 
 options = webdriver.ChromeOptions()
 
