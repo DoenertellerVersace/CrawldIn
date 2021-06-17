096b13f1eedfd36fa68885404e86674035230ca5
KaeseKartoffel <jakob.hadulla@gmail.com>
2021-06-17 23:49:40 +0200

    fixed company identification

--- src/crawler.py
+++ src/crawler.py
@@ -24,25 +24,20 @@ options.add_experimental_option(
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.linkedin.com/login/de?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
 assert "Login" in driver.title
-
+driver.set_window_size(1800, 1000)
+driver.implicitly_wait(10)
 compLinks = {}
 companies = {}
-
-driver.set_window_size(1800, 1000)
-#
-# try:
-#     loginField = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located(By.XPATH("//input[@name='session_key']"))
-#     )
-#     passwdField = driver.find_element_by_xpath("//input[@name='session_password']")
-#
-
-loginField = driver.find_element_by_xpath("//input[@name='session_key']")
-passwdField = driver.find_element_by_xpath("//input[@name='session_password']")
+print('\n')
 
 # session_login = input("Bitte LinkedIn-E-Mail zur Anmeldung eingeben:\n")
 # session_passwd = input("Bitte LinkedIn-Passwort zur Anmeldung eingeben:\n")
 session_search = input("Bitte Unternehmen mit Komma getrennt eingeben:\n")
+# print('\n')
+
+loginField = driver.find_element_by_xpath("//input[@name='session_key']")
+passwdField = driver.find_element_by_xpath("//input[@name='session_password']")
+
 
 compList = session_search.split(",")
 
@@ -52,12 +47,7 @@ passwdField.send_keys("TestPasswort0912")
 
 submit = driver.find_element_by_xpath("//button[@type='submit']")
 submit.click()
-
-time.sleep(4)
-
-# print(driver.get_screenshot_as_file("./pics/pic.png"))
-
-print("\n\n\n\n")
+print("\n")
 
 for company in compList:
     try:
@@ -67,36 +57,39 @@ for company in compList:
         search.clear()
         search.send_keys(company)
         print(f"Suche nach Unternehmen mit Namen '{company}'...")
-
-        time.sleep(1)
-
         search.send_keys(Keys.ENTER)
     except ElementNotInteractableException:
         print('Suchfeld reagiert nicht! WHYYYYY')
     except NoSuchElementException:
         print('Suchfeld nicht gefunden!')
 
-    time.sleep(3)
 
     try:
         result = driver.find_element_by_xpath("//a[@class='app-aware-link']")
         resLink = result.get_attribute("href")
         if "company" in resLink:
             compLinks[company] = resLink
-            print(f"'{company}' gefunden: {resLink[33:-1]}")
+            print(f"'{company}' gefunden: {resLink[33:-1]}\n")
         else:
-            compLinks[company] = False
-            print(f"Für '{company}' wurde nichts gefunden.")
-            driver.get_screenshot_as_file(f"../pics/fail-{company}.png")
+            altResults = driver.find_elements_by_xpath("//a[@class='app-aware-link']")
+            for res in altResults:
+                altResLink = res.get_attribute("href")
+                if "company" in altResLink:
+                    compLinks[company] = altResLink
+                    print(f"'{company}' gefunden: {altResLink[33:-1]}\n")
+                    break
+            # print(f"Für '{company}' wurde nichts gefunden.\n")
+            # driver.get_screenshot_as_file(f"../pics/fail-{company}.png")
     except NoSuchElementException as e:
-        print(f"Für '{company}' wurde nichts gefunden.")
+        print(f"Für '{company}' wurde nichts gefunden.\n")
         driver.get_screenshot_as_file(f"../pics/fail-{company}.png")
-    print("\n")
 
 print(compLinks)
 
 
-print("\n\n\n")
+
+
+print("\n\n")
 
 print(f"Mitarbeiter von {len(compLinks)} Unternehmen werden gezählt:\n")
 
@@ -104,13 +97,13 @@ print(f"Mitarbeiter von {len(compLinks)} Unternehmen werden gezählt:\n")
 for company in compLinks:
     if compLinks[company]:
         print(f"{compLinks[company][33:-1]}...")
-        # driver.get(compLinks[company])
-        # peopleText = ""
-        # try:
-        #     peopleText = driver.find_element_by_xpath(
-        #         "//span[@class='org-top-card-secondary-content__see-all t-normal t-black--light link-without-visited-state link-without-hover-state']").text
-        # except NoSuchElementException as e:
-        #     print(e)
+        driver.get(compLinks[company])
+        peopleText = ""
+        try:
+            peopleText = driver.find_element_by_xpath(
+                "//span[@class='org-top-card-secondary-content__see-all t-normal t-black--light link-without-visited-state link-without-hover-state']").text
+        except NoSuchElementException as e:
+            print(e)
 
 
         driver.get(f"{compLinks[company]}about/")
@@ -119,14 +112,14 @@ for company in compLinks:
             compSize = driver.find_element_by_xpath(
             "//dd[@class='org-about-company-module__company-size-definition-text t-14 t-black--light mb1 fl']").text
         except NoSuchElementException:
-            print("gibts halt nicht")
+            print("keine Information zu finden")
 
         companies[company] = compSize
 
     else:
         companies[company] = False
 
-    time.sleep(2)
+    # time.sleep(2)
 
 nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
@@ -142,7 +135,7 @@ nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 print(companies)
 
-time.sleep(2)
+# time.sleep(2)
 
 driver.close()
 
